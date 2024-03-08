2936c0dff377abda827e1c41f7e04773117005f8
Gabriel Oquendo <oquendogabriel18@gamil.com>
2024-03-08 12:23:30 -0500

    Proteger las rutas

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const ProtectedRoutes = () => {
-  return <div>ProtectedRoutes</div>;
+  const { user, status } = useSelector((state) => state.user);
+  const [userLoaded, setUserLoaded] = useState(false);
+
+  if (!user?._id) {
+    // Si el usuario no está autenticado, redirige a la página de inicio de sesión
+    return <Navigate to="/login" replace />;
+  }
+
+  // Si el usuario está autenticado y completamente cargado, renderiza el contenido protegido
+  return <Outlet />;
 };
 
 export default ProtectedRoutes;
