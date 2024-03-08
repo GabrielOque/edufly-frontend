768399d6da4572b744cb696dd5781629de05477b
Gabriel Oquendo <oquendogabriel18@gamil.com>
2024-03-08 12:17:56 -0500

    logica de la pagina y Cards de las categorias

--- src/components/CardCategory.jsx
+++ src/components/CardCategory.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const CardCategory = () => {
-  return <div>CardCategory</div>;
+const CardCategory = ({ course }) => {
+  const navigate = useNavigate();
+  const handlePageCategory = () => {
+    navigate(`/category/${course.name}/${course._id}`);
+  };
+  return (
+    <div
+      className="w-[250px] text-white cursor-pointer mx-2 my-6 transition-transform transform-gpu hover:scale-105"
+      onClick={handlePageCategory}
+    >
+      <p className="text-white font-bold mb-2 text-xl">{course.name}</p>
+      <img className="object-contain rounded-xl" src={course.image} />
+
+      <p className="mt-2">{course.summary}</p>
+      <div className="w-full text-start font-bold">
+        <p className="mt-2 font-bold">{course.quantitylessons} Lecciones</p>
+        <p>Profesor {course.teacher}</p>
+      </div>
+    </div>
+  );
 };
 
 export default CardCategory;
--- src/pages/ContentCategory.jsx
+++ src/pages/ContentCategory.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import CardDetail from "../components/CardDetail";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { fetchLesson } from "../features/lesson/lessonSlice";
 
 const ContentCategory = () => {
-  return <div>ContentCategory</div>;
+  const dispatch = useDispatch();
+  const { lessons } = useSelector((state) => state.lesson);
+  const { name, id } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchLesson(id));
+  }, []);
+  return (
+    <div
+      className="w-full h-full flex flex-col items-center overflow-y-auto"
+      style={{
+        scrollbarWidth: "thin",
+        scrollbarColor: "#4A5568 #2D3748",
+      }}
+    >
+      <Navbar />
+      <div className="w-[80%] flex flex-col mt-10  h-full ">
+        <h2 className="text-white font-bold text-xl">Curso / {name}</h2>
+        <div className="mt-7 font-bold text-white">
+          <h2 className="font-bold text-xl">Lecciones</h2>
+          <div className="mt-7">
+            {lessons.map((lesson) => (
+              <CardDetail key={lesson._id} lessons={lesson} nameCourse={name} />
+            ))}
+          </div>
+        </div>
+      </div>
+      ;
+    </div>
+  );
 };
 
 export default ContentCategory;
