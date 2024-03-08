1901e6218552152b47303fd3563242279f9de23c
Gabriel Oquendo <oquendogabriel18@gamil.com>
2024-03-08 12:15:53 -0500

    Rutas de las paginas y ruta protectora (ProtectedRoutes)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import ProtectedRoutes from "./components/ProtectedRoutes";
+import ContentCategory from "./pages/ContentCategory";
+import ContentDetails from "./pages/ContentDetails";
+import CertificationsPage from "./pages/CertificationsPage";
+
 function App() {
   return (
     <>
-      <div className="font-bold text-3xl">hola mundo</div>
+      <div className="w-full h-screen bg-[#111115]">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/category/:name/:id" element={<ContentCategory />} />
+            <Route path="/content/:id/:name/" element={<ContentDetails />} />
+            <Route path="/certifications/" element={<CertificationsPage />} />
+          </Route>
+        </Routes>
+      </div>
     </>
   );
 }
